539bf5f9c9ab3ddbee9e56e609a0f88210cd1b4f
Uwe Ohse <uwe@ohse.de>
2022-10-28 14:17:41 +0000

    jobwindow shows labor points, motivation meter and value, and danger display has been modified

--- misc.js
+++ misc.js
@@ -103,37 +103,104 @@ TWDS.market.updateCategory = function (category, data) {
   }
   return ret
 }
-TWDS.jobwindow = {}
-TWDS.jobwindow.initView = function () {
-  this._TWDS_backup_initView()
-  const d = this.window.divMain
-  const id = this.jobId
-  const x = TWDS.jobData['job_' + id]
-  if (d && x && x.job_maxdmg) {
-    const h = Character.health / Math.max(1, Character.maxHealth) / 9
-    const m = TWDS.createElement({
-      nodeName: 'meter',
-      min: 0,
-      optimum: 0,
-      high: h,
-      max: 1,
-      value: x.job_maxdmg / 100
-    })
-    m.style.display = 'block'
-    m.style.width = '100px'
-    m.title = 'an injury costs up to ' + x.job_maxdmg + '% of your maximum health. The meter is calculated based on 9 jobs.'
-    const dan = d.querySelector('.cprog_danger')
-    if (dan) {
-      dan.appendChild(m)
-    }
-  }
-}
 
 TWDS.registerStartFunc(function () {
   MarketWindow.Buy._TWDS_backup_updateCategory = MarketWindow.Buy.updateCategory
   MarketWindow.Buy.updateCategory = TWDS.market.updateCategory
-  JobWindow.prototype._TWDS_backup_initView = JobWindow.prototype.initView
-  JobWindow.prototype.initView = TWDS.jobwindow.initView
+})
+
+// duel protection.
+TWDS.duelprotection = {}
+TWDS.duelprotection.interval = 0
+TWDS.duelprotection.hack = null
+TWDS.duelprotection.updateMouseover = function () {
+  const mand = Character.getMandatoryDuelProtection(true)
+  const opt = Character.getDuelProtection(true)
+  const now = (new window.ServerDate()).getTime()
+  let str = ''
+  let vgl = -1
+  if (mand > now) {
+    str = 'Duel suspension until ' + (new Date(mand)).toLocaleString()
+    TWDS.duelprotection.hack.css({
+      'background-color': '#f446'
+    })
+    vgl = mand
+  } else if (opt > now) {
+    str = 'Duel protection until ' + (new Date(opt)).toLocaleString()
+    vgl = opt
+    TWDS.duelprotection.hack.css({
+      'background-color': '#cc46'
+    })
+  } else {
+    TWDS.duelprotection.hack.css({
+      'background-color': '#7776'
+    })
+  }
+  if (vgl !== -1) {
+    const remain = Math.max((vgl - now) / 1000, 0) // ms
+    const remainstr = remain.formatDuration()
+    if (remain > 0) {
+      str += ' (' + remainstr + ')'
+    }
+    str += '.\n'
+  }
+  const mot1 = Character.duelMotivation
+  const mot2 = Character.npcDuelMotivation
+  str += '<p>Duel motivation</p>'
+  str += '<table>'
+  str += '<tr><th>PC'
+  str += '<td><meter min="0" low="0" optimum="1" high="1" max="1" value="' + mot1 + '"></meter>'
+  str += '<td>' + parseInt(100 * mot1)
+  str += '<tr><th>NPC'
+  str += '<td><meter min="0" low="0" optimum="1" high="1" max="1" value="' + mot2 + '"></meter>'
+  str += '<td>' + parseInt(100 * mot2)
+  str += '</table>'
+  str += '<p>The NPC duel motivation is valid after you opened the duels menu. Unfortunately the data is not updated earlier.</p>'
+  TWDS.duelprotection.hack.addMousePopup(str)
+}
+TWDS.duelprotection.init = function (active) {
+  if (!active) {
+    if (TWDS.duelprotection.interval) {
+      clearInterval(TWDS.duelprotection.interval)
+      TWDS.duelprotection.interval = 0
+    }
+    if (TWDS.duelprotection.hack !== null) {
+      TWDS.duelprotection.hack.remove()
+    }
+    TWDS.duelprotection.hack.removeMousePopup()
+    TWDS.duelprotection.hack.remove()
+    return
+  }
+
+  if (TWDS.settings.misc_duelprotection_display) {
+    const cl = $('#ui_character_container')
+    const hack = $("<div id='TWDS_duelprotection_hack' />")
+    hack.css({
+      position: 'relative',
+      background: "url('https://westen.innogamescdn.com/images/interface/dock_icons.png?4')",
+      width: '52px',
+      height: '52px',
+      cursor: 'pointer',
+      'background-size': 'auto',
+      display: 'inline-block',
+      right: '-4px',
+      top: '24px',
+      'background-position-x': '-52px',
+      'background-position-y': '-52px',
+      'border-radius': '50%',
+      'background-color': '#7776'
+    })
+    $(cl).append(hack)
+    $(hack).hover(TWDS.duelprotection.updateMouseover)
+    TWDS.duelprotection.hack = hack
+    // update the bg color, too.
+    TWDS.duelprotection.updateMouseover()
+    TWDS.duelprotection.interval = setInterval(TWDS.duelprotection.updateMouseover, 60 * 1000)
+  }
+}
+TWDS.registerStartFunc(function () {
+  TWDS.registerSetting('bool', 'misc_duelprotection_display',
+    'Show a duel protection overlay on your image', true, TWDS.duelprotection.init)
 })
 
 // vim: tabstop=2 shiftwidth=2 expandtab
