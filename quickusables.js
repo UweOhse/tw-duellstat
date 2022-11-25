c5ab5f0e9419ee9465e36b6dfd898a93dd84306e
Uwe Ohse <uwe@ohse.de>
2022-11-25 17:53:09 +0000

    make classification func for market filter, and combine duel/fortbattle buffs

--- quickusables.js
+++ quickusables.js
@@ -8,6 +8,39 @@ TWDS.quickusables.openwrapper = function (eventdata) {
 }
 
 TWDS.quickusables.usables = null
+TWDS.quickusables.catnames = {
+  energy: TWDS._('QUICKUSABLES_ENERGY', 'Energy'),
+  health: TWDS._('QUICKUSABLES_HEALTH', 'Health'),
+  workmotivation: TWDS._('QUICKUSABLES_WMOT', 'Work motivation'),
+  duelmotivation: TWDS._('QUICKUSABLES_DMOT', 'Duel motivation'),
+  drop: TWDS._('QUICKUSABLES_DROP', 'Drop chance'),
+  experience: TWDS._('QUICKUSABLES_XP', 'Experience'),
+  luck: TWDS._('QUICKUSABLES_LUCK', 'Luck'),
+  money: TWDS._('QUICKUSABLES_MONEY', 'Money'),
+  duel: TWDS._('QUICKUSABLES_DUEL', 'Duel'),
+  fortbattle: TWDS._('QUICKUSABLES_FB', 'Fort battle'),
+  multiplayer: TWDS._('QUICKUSABLES_MPI', 'Multiplayer'),
+  movement: TWDS._('QUICKUSABLES_MOVEMENT', 'Movement'),
+  laborpoints: TWDS._('QUICKUSABLES_LP', 'Labor points')
+}
+TWDS.quickusables.getcatdesc = function (cat) {
+  if (cat in TWDS.quickusables.catnames) { return TWDS.quickusables.catnames[cat] }
+  return cat
+}
+TWDS.quickusables.hascat = function (cat) {
+  return (cat in TWDS.quickusables.catnames)
+}
+TWDS.quickusables.getcategories = function () {
+  if (TWDS.quickusables.usables === null) {
+    TWDS.quickusables.initusables()
+  }
+  const out = []
+  for (const i of Object.keys(TWDS.quickusables.usables)) {
+    if (i.match(/_x$/)) continue
+    out.push(i)
+  }
+  return out
+}
 TWDS.quickusables.initusables = function () {
   TWDS.quickusables.usables = {
     energy: [],
@@ -18,12 +51,12 @@ TWDS.quickusables.initusables = function () {
     experience: [],
     money: [],
     luck: [],
-    dueldamage: [],
-    fortbattledamage: [],
+    duel: [],
+    fortbattle: [],
+    fortbattle_x: [],
     multiplayer: [],
-    waytime: [],
-    speed: [],
-    laborpoints: []
+    laborpoints: [],
+    movement: []
   }
   const clean = function (str) {
     str = str.replace(/([0-9]+)-([0-9]+)/, '')
@@ -46,14 +79,62 @@ TWDS.quickusables.initusables = function () {
   doit(2466, 'drop')
   doit(2467, 'experience')
   doit(2468, 'money')
-  doit(2741, 'fortbattledamage', 0)
   doit(2741, 'multiplayer', 1)
   doit(2741, 'multiplayer', 2)
-  doit(1901, 'dueldamage')
-  doit(1926, 'waytime')
-  doit(1927, 'speed')
+  doit(1926, 'movement')
+  doit(1927, 'movement')
   doit(1940, 'laborpoints')
   doit(1946, 'health', 1)
+  doit(1901, 'duel')
+  doit(1863, 'duel', 0)
+  doit(1863, 'duel', 1)
+  doit(1864, 'duel', 0)
+  doit(1864, 'duel', 1)
+  doit(1871, 'duel', 1)
+  doit(1872, 'duel', 1)
+  doit(51125, 'duel', 0)
+  doit(51125, 'duel', 1)
+  doit(2741, 'fortbattle', 0)
+  doit(1873, 'fortbattle', 1) // gemüsetasche, leiten
+  doit(1946, 'fortbattle', 0) // amulett, ausweichen
+  doit(1946, 'fortbattle', 1) // amulett, lp
+  doit(2525, 'fortbattle', 1) // zaubertinte, fallen stellen
+  doit(2525, 'fortbattle', 2) // zaubertinte, verstecken
+  doit(1940, 'fortbattle_x', 0) // stück torte, arbeitspunkte
+}
+TWDS.quickusables.match = function (item, cat) {
+  if (TWDS.quickusables.usables === null) {
+    TWDS.quickusables.initusables()
+  }
+  if (!('usetype' in item)) return false
+  if (item.usetype === 'none') return false
+  const ub = item.usebonus
+  const searchstrings = TWDS.quickusables.usables[cat]
+  if (searchstrings === undefined) { // no known cat
+    return false
+  }
+  let found = false
+  for (let i = 0; i < ub.length; i++) {
+    const b = ub[i]
+    for (let j = 0; j < searchstrings.length; j++) {
+      if (b.search(searchstrings[j]) !== -1) {
+        found = true
+        break
+      }
+    }
+  }
+  if (!found) return false
+  if ((cat + '_x') in TWDS.quickusables.usables) {
+    const exclusions = TWDS.quickusables.usables[cat + '_x']
+    for (let k = 0; k < ub.length; k++) {
+      for (let x = 0; x < exclusions.length; x++) {
+        if (ub[k].toLocaleLowerCase().search(exclusions[x].toLocaleLowerCase()) !== -1) {
+          return false
+        }
+      }
+    }
+  }
+  return true
 }
 TWDS.quickusables.showusables = function (choice) {
   console.log('C', choice)
@@ -76,7 +157,24 @@ TWDS.quickusables.showusables = function (choice) {
     const found = Bag.search(translatedchoices[j])
     for (let i = 0; i < found.length; i++) {
       if ('usetype' in found[i].obj && found[i].obj.usetype !== 'none') {
-        filtered.push(found[i])
+        console.log('SEARCH', j, translatedchoices[j], 'FOUND', found[i])
+        let exclude = false
+        if ((choice + '_x') in TWDS.quickusables.usables) {
+          const ub = found[i].obj.usebonus
+          const exclusions = TWDS.quickusables.usables[choice + '_x']
+          console.log('XX', exclusions)
+          for (let k = 0; k < ub.length; k++) {
+            for (let x = 0; x < exclusions.length; x++) {
+              if (ub[k].toLocaleLowerCase().search(exclusions[x].toLocaleLowerCase()) !== -1) {
+                console.log('SEARCH', j, translatedchoices[j], 'EXCLUSE', found[i])
+                exclude = true
+              }
+            }
+          }
+        }
+        if (!exclude) {
+          filtered.push(found[i])
+        }
       }
     }
   }
@@ -85,9 +183,11 @@ TWDS.quickusables.showusables = function (choice) {
 }
 TWDS.quickusables.open = function (eventdata) {
   console.log('quickusables.open')
-  const sb = (new west.gui.Selectbox(true)).addListener(function (choice) {
-    TWDS.quickusables.showusables(choice)
-  })
+  const sb = (new west.gui.Selectbox(true))
+    .setHeight('347px')
+    .addListener(function (choice) {
+      TWDS.quickusables.showusables(choice)
+    })
   sb.addClass('TWDS_quickusableshelper')
   sb.addItem('energy', TWDS._('QUICKUSABLES_ENERGY', 'Energy'))
   sb.addItem('health', TWDS._('QUICKUSABLES_HEALTH', 'Health'))
@@ -99,11 +199,10 @@ TWDS.quickusables.open = function (eventdata) {
     ['experience', TWDS._('QUICKUSABLES_XP', 'Experience')],
     ['luck', TWDS._('QUICKUSABLES_LUCK', 'Luck')],
     ['luck', TWDS._('QUICKUSABLES_MONEY', 'Money')],
-    ['dueldamage', TWDS._('QUICKUSABLES_DUELDAMAGE', 'Duel damage')],
-    ['fortbattledamage', TWDS._('QUICKUSABLES_FBDAMAGE', 'Fort battle damage')],
+    ['duel', TWDS._('QUICKUSABLES_DUEL', 'Duel')],
+    ['fortbattle', TWDS._('QUICKUSABLES_FB', 'Fort battle')],
     ['multiplayer', TWDS._('QUICKUSABLES_MPI', 'Multiplayer')],
-    ['waytime', TWDS._('QUICKUSABLES_WAYTIME', 'Waytime shortening')],
-    ['speed', TWDS._('QUICKUSABLES_SPEED', 'Speed')],
+    ['movement', TWDS._('QUICKUSABLES_MOVEMENT', 'Movement')],
     ['laborpoints', TWDS._('QUICKUSABLES_LP', 'Labor points')]
   ]
   x.sort(function (a, b) {
