ad023c387b64812c20c17564107be4039bbee015
Uwe Ohse <uwe@ohse.de>
2022-11-25 17:24:22 +0000

    smartstart and luckp display

--- jobwindow.js
+++ jobwindow.js
@@ -37,6 +37,10 @@ TWDS.jobwindow = {}
 
 TWDS.jobwindow.initView = function () {
   this._TWDS_backup_initView()
+  return TWDS.jobwindow.initView2.call(this)
+} // for TWDS.reload()
+TWDS.jobwindow.initView2 = function () {
+  console.log('TWDS.jobwindow.initView', this)
   const d = this.window.divMain
   const old = TWDS.q1('.TWDS_jobwindow_setbuttons', d)
   if (old) {
@@ -79,6 +83,61 @@ TWDS.jobwindow.initView = function () {
     if (p) {
       p.style.display = 'block'
     }
+  }
+  if (TWDS.settings.jobwindow_show_luckp) {
+    const hack = function (what, basis) {
+      const b = TWDS.q1('.job_durationbar_' + what, d)
+      const e = TWDS.createEle({
+        nodeName: 'div',
+        className: 'TWDS_jw_luck',
+        textContent: (100 * basis[what].p_luck).toFixed(0) + '%'
+      })
+      b.appendChild(e)
+    }
+    hack('short', this.job.basis)
+    hack('middle', this.job.basis)
+    hack('long', this.job.basis)
+  }
+  if (TWDS.settings.jobwindow_smart_start) {
+    const o = TWDS.q('.TWDS_smartstart', d)
+    if (o) {
+      for (let i = 0; i < o.length; i++) { o[i].remove() }
+    }
+    const c = function (en, pa, that) {
+      let dur = 15
+      if (en === 5) {
+        dur = that.job.basis.middle.duration
+      } else if (en === 12) {
+        dur = that.job.basis.long.duration
+      } else {
+        dur = that.job.basis.short.duration
+      }
+      return TWDS.createEle({
+        nodeName: 'button',
+        title: TWDS._('JOBWINDOW_SSB12_TITLE',
+          'Start as many jobs as possible in the same motivation class'),
+        textContent: 'dummy',
+        className: 'TWDS_smartstart',
+        dataset: {
+          x: that.x,
+          y: that.y,
+          jobid: that.jobId,
+          base: en,
+          duration: dur
+        },
+        beforeend: pa,
+        onclick: function (e) {
+          return TWDS.jobwindow.smartstart(e)
+        }
+      })
+    }
+    // i tried west.gui.button, but the onclick handler doesn't get the event, and so i can't prevent
+    // a click on the parent column, which has a click handler.
+    TWDS.jobwindow.ssb1 = c(1, TWDS.q1('.job_durationbar_short', d), this)
+    TWDS.jobwindow.ssb2 = c(5, TWDS.q1('.job_durationbar_middle', d), this)
+    TWDS.jobwindow.ssb3 = c(12, TWDS.q1('.job_durationbar_long', d), this)
+    TWDS.jobwindow.ssb3.title = TWDS._('JOBWINDOW_SSB3_TITLE',
+      'Start as many jobs as possible')
   }
   if (TWDS.settings.jobwindow_show_jobpoints) {
     const progressthing = d.querySelector('.job_progress_jobstars')
@@ -220,7 +279,11 @@ TWDS.jobwindow.updateMotivationMeter = function (o) {
   const info = $('.TWDS_jobmotivation_info', o.DOM)
   if (info.length) { info[0].textContent = mot }
   const met = $('.TWDS_jobmotivation_meter', o.DOM)
+
   if (met.length) {
+    let tonextborder = mot % 25
+    tonextborder -= 1
+    if (tonextborder < 0) tonextborder = 25
     met.value = mot
     // that oh so good designed <meter> element doesn't allow me to change the colors
     // without a lot of browser dependend overhead (meter::--webkit-meter-...).
@@ -231,13 +294,40 @@ TWDS.jobwindow.updateMotivationMeter = function (o) {
     // 26 is shown as 0, 50 as 25
     //  1 is shown as 0, 25 as 25
     //  0..
-    let t = mot % 25
-    t -= 1
-    if (t < 0) t = 25
-    met[0].value = t
+    met[0].value = tonextborder
     met[0].min = 0
     met[0].max = 25
   }
+  const hasa = Premium.hasBonus('automation')
+  let maxpossible = 4
+  if (hasa) { maxpossible = 9 }
+  // now the smart buttons.
+  let tonextborder = mot % 25
+  if (tonextborder === 0) { tonextborder = 25 }
+  let t = Math.min(maxpossible, tonextborder, Character.energy)
+  TWDS.jobwindow.ssb1.textContent = t + 'x'
+  TWDS.jobwindow.ssb1.dataset.smartstart = t
+
+  t = Math.min(maxpossible, Math.ceil(tonextborder / 5), Math.floor(Character.energy / 5))
+  TWDS.jobwindow.ssb2.textContent = t + 'x'
+  TWDS.jobwindow.ssb2.dataset.smartstart = t
+
+  t = Math.min(maxpossible, Math.floor(Character.energy / 12))
+  TWDS.jobwindow.ssb3.textContent = t + 'x'
+  TWDS.jobwindow.ssb3.dataset.smartstart = t
+}
+TWDS.jobwindow.smartstart = function (e, y) {
+  e.preventDefault()
+  e.stopPropagation()
+  const ds = e.target.dataset
+  let amount = ds.smartstart
+  const dur = parseInt(ds.duration)
+  const tasks = []
+  while (amount--) {
+    tasks.push(new window.TaskJob(ds.jobid, ds.x, ds.y, dur))
+  }
+  TaskQueue.add(tasks)
+  return false
 }
 TWDS.jobwindow.updateMotivation = function (jobdata) {
   JobWindow.prototype._TWDS_backup_updateMotivation.apply(this, arguments)
@@ -249,6 +339,10 @@ TWDS.registerSetting('bool', 'jobwindow_show_motivation',
   'Show the exact current motivation in the job window', true, null, 'Jobwindow')
 TWDS.registerSetting('bool', 'jobwindow_show_jobpoints',
   'Show the job points in the job window', true, null, 'Jobwindow')
+TWDS.registerSetting('bool', 'jobwindow_smart_start',
+  'Add buttons to start multiple jobs', true, null, 'Jobwindow')
+TWDS.registerSetting('bool', 'jobwindow_show_luckp',
+  'Show the chance to make a lucky find.', true, null, 'Jobwindow')
 
 TWDS.registerStartFunc(function () {
   TWDS.registerSetting('info', 'AAAAA',
