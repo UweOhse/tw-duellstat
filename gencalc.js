00b7e1192469f879c5eb1b15b9e81b9371d5512f
Uwe Ohse <uwe@ohse.de>
2023-02-21 09:41:36 +0000

    extend gencalc

--- gencalc.js
+++ gencalc.js
@@ -1,10 +1,22 @@
 // vim: tabstop=2 shiftwidth=2 expandtab
 
-TWDS.genCalc = function (bonusNames, skills) {
-  const availableSets = west.item.Calculator.filterUnavailableSets(west.storage.ItemSetManager.getAll())
-  const bestItems = TWDS.genCalc.getBestItems(bonusNames, skills)
+TWDS.genCalc = function (bonusNames, skills, include) {
+  const o = TWDS.genCalc.exec(bonusNames, skills, include)
+  return o.combos[0][1]
+}
+TWDS.genCalc.exec = function (bonusNames, skills, include) {
+  if (!include) include = 1 // owned
+  const availableSets = TWDS.genCalc.setfilter(include)
 
-  console.log('bi', bestItems)
+  const bires = TWDS.genCalc.getBestItems(bonusNames, skills, include)
+  const bestItems = bires[0]
+  const goodItems = bires[1]
+  console.log('bestItems', bestItems)
+  console.log('goodItems', goodItems)
+
+  for (let i = 0; i < bestItems.length; i++) {
+    bestItems[i] = ItemManager.get(bestItems[i])
+  }
   console.log('bestItems', TWDS.describeItemCombo(bestItems))
 
   const bestItemsContainer = new west.item.ItemSetContainer()
@@ -27,8 +39,10 @@ TWDS.genCalc = function (bonusNames, skills) {
 
   let bestPoints = -1
   let best = null
+  const setsandpoints = []
   for (let i = 0; i < sets.length; i++) {
     const spd = TWDS.genCalc.calcCombinedSet(sets[i], bonusNames, skills)
+    setsandpoints[i] = [spd, sets[i]]
     if (spd > bestPoints) {
       bestPoints = spd
       best = sets[i]
@@ -36,8 +50,96 @@ TWDS.genCalc = function (bonusNames, skills) {
         TWDS.genCalc.getItems(sets[i]), spd)
     }
   }
+  setsandpoints.sort(function (a, b) {
+    return (b[0] - a[0])
+  })
   console.log('best', bestPoints, best)
-  return TWDS.genCalc.getItems(best)
+  console.log('bestx', setsandpoints)
+  console.log('bestItems', bestItems)
+  for (let i = 0; i < bestItems.length; i++) {
+    bestItems[i] = bestItems[i].getId()
+  }
+  const ret = {
+    combos: [],
+    items: goodItems
+  }
+  const merk = {}
+  for (let i = 0; i < setsandpoints.length; i++) {
+    const one = setsandpoints[i][1]
+    const pts = setsandpoints[i][0]
+    const items = TWDS.genCalc.getItems(one)
+    // console.log("S&P",one,pts,items);
+    let flag = 0
+    for (let j = 0; j < items.length; j++) {
+      const k = items[j]
+      if (k in merk) {
+        flag++
+      }
+    }
+    let limit = 10 - ret.combos.length
+    if (limit < 4) limit = 4
+    //     console.log('CHECK', pts, one, items, merk, 'check', flag, '<', limit)
+    if (flag < limit) {
+      ret.combos.push([pts, items])
+      for (let j = 0; j < items.length; j++) {
+        const itno = items[j]
+        if (!(itno in merk)) merk[itno] = 0
+        merk[itno]++
+      }
+    }
+  }
+  return ret
+}
+TWDS.genCalc.setfilter = function (include) {
+  console.log('INC', include)
+  const availableSets = []
+  const allSets = west.storage.ItemSetManager.getAll()
+  for (let i = 0; i < allSets.length; i++) {
+    const set = allSets[i]
+    const items = set.items
+    const o = []
+    for (let j = 0; j < items.length; j++) {
+      const itemId = items[j] // base item ids
+      const item = ItemManager.getByBaseId(itemId)
+      if ((include & 16) || item.wearable()) {
+        if (include & 1) { // owned
+          const x = Bag.getItemsIdsByBaseItemId(itemId)
+          if (x.length) {
+            o.push(x[0]) // itemid
+            continue
+          }
+          if (Wear.carries(itemId)) {
+            o.push(Wear.getByBaseId(itemId).getId())
+            continue
+          }
+        }
+        if (include & 2) { // autionable
+          if (item.auctionable) {
+            o.push(item.getId())
+            continue
+          }
+        }
+        if (include & 4) {
+          if (item.tradeable && item.traderlevel < 66 && item.traderlevel !== null) {
+            o.push(item.getId())
+            continue
+          }
+        }
+        if (include & 8) {
+          o.push(item.getId())
+          continue
+        }
+      }
+    }
+    if (o.length) {
+      availableSets.push(new west.item.ItemSet({
+        key: set.key,
+        items: o,
+        bonus: set.bonus
+      }))
+    }
+  }
+  return availableSets
 }
 
 TWDS.genCalc.filterUneffectiveSets = function (sets, bonusNames, skills) {
@@ -67,7 +169,19 @@ TWDS.genCalc.getItems = function (set) {
   for (const oneset of Object.values(set.sets)) {
     for (let i = 0; i < oneset.items.length; i++) { it.push(oneset.items[i]) }
   }
-  return it
+  const o = []
+  const slots = Wear.slots
+  for (let i = 0; i < slots.length; i++) {
+    const sl = slots[i]
+    for (let j = 0; j < it.length; j++) {
+      const one = ItemManager.get(it[j])
+      if (one.type === sl) {
+        o[i] = it[j]
+      }
+    }
+  }
+
+  return o
 }
 
 TWDS.genCalc.calcCombinedSet = function (set, bonusNames, skills) {
@@ -158,10 +272,60 @@ TWDS.genCalc.beatsBestItems = function (set, bestItems, bonusNames, skills) {
   return setSpeed > biSpeed // || setData.speedBonus > bestItemSpeedBonus
 }
 
-TWDS.genCalc.getBestItems = function (bonusNames, skills) {
+TWDS.genCalc.getBestItems = function (bonusNames, skills, include) {
   const bestItems = {}
   const result = []
-  const itemsByBase = Bag.getItemsIdsByBaseItemIds()
+  const itemsByBase = {}
+  const add = function (base, lng) {
+    const x = ItemManager.getByBaseId(base)
+    if (!x) return
+    if (!x.wearable() && !(include & 16)) return
+    if (base in itemsByBase) {
+      itemsByBase[base].push(lng)
+    } else {
+      itemsByBase[base] = [lng]
+    }
+  }
+
+  const all = ItemManager.getAll()
+  for (const bid in all) {
+    if (include & 1) {
+      const y = Bag.getItemsIdsByBaseItemId(bid)
+      if (y.length) {
+        add(bid, y[0])
+      }
+    }
+    if (include & 2) { // auction
+      const item = all[bid]
+      if (item.auctionable) {
+        add(bid, item.item_id)
+      }
+    }
+    if (include & 4) { // trader
+      const item = all[bid]
+      if (item.tradeable && item.traderlevel < 66 && item.traderlevel !== null) {
+        add(bid, item.item_id)
+      }
+    }
+    if (include & 8) {
+      add(bid, all[bid].item_id)
+    }
+  }
+  if (include & 1) { // owned shall include worn
+    const slots = Wear.slots
+    for (let i = 0; i < slots.length; i++) {
+      const sl = slots[i]
+      const it = Wear.get(sl)
+      if (it) {
+        console.log('ADD8', it.obj.name)
+        add(it.obj.item_base_id, it.obj.item_id)
+      }
+    }
+  }
+
+  delete itemsByBase[41999] // allmighty ...
+  delete itemsByBase[1337] // sword of a thousand truths
+
   west.common.forEach(itemsByBase, function (items, baseId) {
     const item = ItemManager.get(items[0])
     const type = item.getType()
@@ -176,7 +340,11 @@ TWDS.genCalc.getBestItems = function (bonusNames, skills) {
     bestItems[type] = bestItems[type] || []
     // const value = item.getValue(skills)
     const value = TWDS.genCalc.getGenValues(item, bonusNames, skills)
-    if ((value.theBonus || value.theSecondary) && item.wearable()) {
+    if (item.getId() === 53065000 || item.getId() === 52278000) {
+      console.log('I', item, item.getId, value)
+    }
+
+    if ((value.theBonus || value.theSecondary) && (item.wearable() || (include & 16))) {
       bestItems[type].push({
         item: item,
         id: item.getId(),
@@ -185,29 +353,26 @@ TWDS.genCalc.getBestItems = function (bonusNames, skills) {
       })
     }
   })
-  west.common.forEach(bestItems, function (items, type) {
-    let wearItem = Wear.get(type)
-    if (wearItem) {
-      wearItem = ItemManager.get(wearItem.getId())
-      items.push({
-        item: wearItem,
-        id: wearItem.getId(),
-        base_id: wearItem.getItemBaseId(),
-        value: TWDS.genCalc.getGenValues(wearItem, bonusNames, skills)
+  const other = {}
+  const slots = Wear.slots
+  for (let i = 0; i < slots.length; i++) {
+    const type = slots[i]
+    if (type in bestItems) {
+      bestItems[type].sort(function (a, b) {
+        const aSpeed = TWDS.genCalc.calc2(a.value.theBonus, a.value.theSecondary)
+        const bSpeed = TWDS.genCalc.calc2(b.value.theBonus, b.value.theSecondary)
+        return (bSpeed - aSpeed)
       })
+      other[type] = []
+      if (bestItems[type].length) { // "recipe" has length 0
+        result.push(bestItems[type][0].item.getId())
+        for (let i = 0; i < bestItems[type].length && i < 10; i++) {
+          other[type].push(bestItems[type][i].item.getId())
+        }
+      }
     }
-    // return (100 + 100 * tmp.speed + tmp.ride) * (1 + tmp.speedBonus)
-    bestItems[type] = items.sort(function (a, b) {
-      const aSpeed = TWDS.genCalc.calc2(a.value.theBonus, a.value.theSecondary)
-      const bSpeed = TWDS.genCalc.calc2(b.value.theBonus, b.value.theSecondary)
-      return (bSpeed - aSpeed)
-    })
-    if (bestItems[type].length) {
-      console.log('type', type, bestItems[type][0])
-      result.push(bestItems[type][0].item)
-    }
-  })
-  return result
+  }
+  return [result, other]
 }
 TWDS.genCalc.calc2 = function (theBonus, theSecondary) {
   return theBonus + theSecondary
@@ -225,13 +390,27 @@ TWDS.genCalc.getGenValues = function (item, bonusNames, skills) {
   const skillAddition = {}
   let skillArr
   let i
-  const memo = 'TWDSgenCalc.' + JSON.stringify(bonusNames) + '.' + JSON.stringify(skills)
+  let memo = 'TWDSgC'
+  let debug = false
+  if (item.item_id === 51006000) { debug = true; console.log('GG', item) }
+  for (const bonusname in bonusNames) {
+    bonusNames[bonusname] = parseInt(bonusNames[bonusname])
+    if (bonusNames[bonusname]) {
+      memo = memo + '.' + bonusname + '=' + bonusNames[bonusname]
+    }
+  }
+  for (const skillname in skills) {
+    skills[skillname] = parseInt(skills[skillname])
+    if (skills[skillname]) {
+      memo = memo + '.' + skillname + '=' + skills[skillname]
+    }
+  }
 
   let bonusExtractor
   let affectedSkills
 
   if (!('_memo' in item)) item._memo = {} // this happens.
-
+  if (debug) item._memo = {}
   if (item._memo[memo]) { return item._memo[memo] }
 
   for (skill in skills) {
@@ -239,6 +418,7 @@ TWDS.genCalc.getGenValues = function (item, bonusNames, skills) {
     attr = CharacterSkills.getAttributeKey4Skill(skill)
     attributes[attr] = (attributes[attr] || 0) + 1
   }
+
   for (attr in item.bonus.attributes) {
     if (!attributes[attr]) { continue }
     skillArr = CharacterSkills.getSkillKeys4Attribute(attr)
@@ -246,23 +426,26 @@ TWDS.genCalc.getGenValues = function (item, bonusNames, skills) {
       if (skills[skillArr[i]]) { skillAddition[skillArr[i]] = item.bonus.attributes[attr] }
     }
   }
+
+  if (debug) console.log('HIB?', item.hasItemBonus())
   if (item.hasItemBonus()) {
     bonusExtractor = new west.item.BonusExtractor(Character, item.getItemLevel())
     for (i = 0; i < item.bonus.item.length; i++) {
       const b = bonusExtractor.getExportValue(item.bonus.item[i])
 
+      if (debug) console.log('IB', i, b.key, b.value)
       if (b.key in bonusNames) { theBonus += b.value * bonusNames[b.key] } else {
         const old = theBonus
         if (b.key === 'fort_defense' && 'fbdefense' in bonusNames) {
           theBonus += b.value * bonusNames.fbdefense
         }
-        if (b.key === 'fort_defense_sector' && 'fbdefense' in bonusNames) {
+        if (b.key === 'fort_defense_sector' && 'fbdefense_sector' in bonusNames) {
           theBonus += b.value * bonusNames.fbdefense
         }
         if (b.key === 'fort_offense' && 'fboffense' in bonusNames) {
           theBonus += b.value * bonusNames.fboffense
         }
-        if (b.key === 'fort_offense_sector' && 'fboffense' in bonusNames) {
+        if (b.key === 'fort_offense_sector' && 'fboffense_sector' in bonusNames) {
           theBonus += b.value * bonusNames.fboffense
         }
         if (b.key === 'fort_resistance' && 'fbresistance' in bonusNames) {
@@ -276,7 +459,6 @@ TWDS.genCalc.getGenValues = function (item, bonusNames, skills) {
           break
         }
       }
-
       affectedSkills = bonusExtractor.getAffectedSkills(item.bonus.item[i])
       for (skill in affectedSkills) {
         if (!(skill in skills)) { continue }
@@ -284,6 +466,57 @@ TWDS.genCalc.getGenValues = function (item, bonusNames, skills) {
       }
     }
   }
+  if ('fortbattle' in item.bonus) {
+    if (item.bonus.fortbattle.offense && 'fboffense' in bonusNames) {
+      theBonus += item.bonus.fortbattle.offense * bonusNames.fboffense
+    }
+    if (item.bonus.fortbattle.defense && 'fbdefense' in bonusNames) {
+      theBonus += item.bonus.fortbattle.defense * bonusNames.fbdefense
+    }
+    if (item.bonus.fortbattle.resistance && 'fbresistance' in bonusNames) {
+      theBonus += item.bonus.fortbattle.resistance * bonusNames.fbresistance
+    }
+  }
+  if ('fortbattlesector' in item.bonus) {
+    if (item.bonus.fortbattlesector.offense && 'fboffense_sector' in bonusNames) {
+      theBonus += item.bonus.fortbattlesector.offense * bonusNames.fboffense_sector
+    }
+    if (item.bonus.fortbattlesector.defense && 'fbdefense_sector' in bonusNames) {
+      theBonus += item.bonus.fortbattlesector.defense * bonusNames.fbdefense_sector
+    }
+    if (item.bonus.fortbattlesector.damage && 'fbdamage' in bonusNames) {
+      theBonus += item.bonus.fortbattlesector.damage * bonusNames.fbdamage
+    }
+  }
+  /*
+  for (const [k, factor] of Object.entries(bonusNames)) {
+    if (k === 'fbdefense') {
+      if ('fortbattle' in bonus && 'defense' in bonus.fortbattle) { boni.theBonus += bonus.fortbattle.defense * factor }
+    } else if (k === 'fboffense') {
+      if ('fortbattle' in bonus && 'offense' in bonus.fortbattle) { boni.theBonus += bonus.fortbattle.offense * factor }
+    } else if (k === 'fbdefense_sector') {
+      if ('fortbattlesector' in bonus && 'defense' in bonus.fortbattlesector) { boni.theBonus += bonus.fortbattlesector.defense * factor }
+    } else if (k === 'fboffense_sector') {
+      // console.log('NOT IMPL', 'gsBGV0', k, bonus,set, set.key)
+      // console.log('NOT IMPL', 'gsBGV0', k, bonus)
+      if ('fortbattlesector' in bonus && 'offense' in bonus.fortbattlesector) { boni.theBonus += bonus.fortbattlesector.offense * factor }
+    } else if (k === 'fbdamage') {
+      if ('fortbattle' in bonus && 'damage' in bonus.fortbattle) { boni.theBonus += bonus.fortbattle.damage * factor }
+    } else if (k === 'fbresistance') {
+      if ('fortbattle' in bonus && 'resistance' in bonus.fortbattle) { boni.theBonus += bonus.fortbattle.resistance * factor }
+    } else if (k === 'joball') {
+      if ('job' in bonus && 'all' in bonus.job) { boni.theBonus += bonus.job.all * factor }
+    } else if (k === 'melee') { // do nothing
+    } else if (k === 'range') { // do nothing
+    } else {
+      boni.theBonus += bonus[k] * factor
+    }
+    if (isNaN(boni.theBonus)) {
+      console.log('isNaN trap', 'gsBGV0', k, bonus)
+      break
+    }
+  }
+  */
   for (skill in skills) {
     if (item.bonus.skills[skill] || skillAddition[skill]) {
       value += skills[skill] * ((item.bonus.skills[skill] || 0) + (skillAddition[skill] || 0))
@@ -360,12 +593,26 @@ TWDS.genCalc.getSetBonusGenValues = function (set, bonusNames, skills) {
       if ('fortbattle' in bonus && 'defense' in bonus.fortbattle) { boni.theBonus += bonus.fortbattle.defense * factor }
     } else if (k === 'fboffense') {
       if ('fortbattle' in bonus && 'offense' in bonus.fortbattle) { boni.theBonus += bonus.fortbattle.offense * factor }
+    } else if (k === 'fbdefense_sector') {
+      if ('fortbattlesector' in bonus && 'defense' in bonus.fortbattlesector) { boni.theBonus += bonus.fortbattlesector.defense * factor }
+    } else if (k === 'fboffense_sector') {
+      // console.log('NOT IMPL', 'gsBGV0', k, bonus,set, set.key)
+      // console.log('NOT IMPL', 'gsBGV0', k, bonus)
+      if ('fortbattlesector' in bonus && 'offense' in bonus.fortbattlesector) { boni.theBonus += bonus.fortbattlesector.offense * factor }
     } else if (k === 'fbdamage') {
       if ('fortbattle' in bonus && 'damage' in bonus.fortbattle) { boni.theBonus += bonus.fortbattle.damage * factor }
     } else if (k === 'fbresistance') {
       if ('fortbattle' in bonus && 'resistance' in bonus.fortbattle) { boni.theBonus += bonus.fortbattle.resistance * factor }
+    } else if (k === 'joball') {
+      if ('job' in bonus && 'all' in bonus.job) { boni.theBonus += bonus.job.all * factor }
+    } else if (k === 'melee') { // do nothing
+    } else if (k === 'range') { // do nothing
     } else {
       boni.theBonus += bonus[k] * factor
+    }
+    if (isNaN(boni.theBonus)) {
+      console.log('isNaN trap', 'gsBGV0', k, bonus)
+      break
     }
   }
 
@@ -380,7 +627,9 @@ TWDS.genCalc.getSetBonusGenValues = function (set, bonusNames, skills) {
 TWDS.genCalc.ItemSet = {}
 
 TWDS.genCalc.ItemSet.getMergedBonus = function (set) {
+  if (set.key === '2018_doldenset') console.log('XXX', 'TRUE')
   if (set._mergedBonus) { return set._mergedBonus }
+  if (set.key === '2018_doldenset') console.log('XXX', 'TRUE2')
   const bonus = {
     damage: 0,
     dollar: 0,
@@ -396,6 +645,7 @@ TWDS.genCalc.ItemSet.getMergedBonus = function (set) {
     experience: 0
   }; const bonusObjects = TWDS.genCalc.ItemSet.getMergedStages(set); let i; let b; const bonusExtractor = new west.item.BonusExtractor(Character)
   const merge = function (b, value) {
+    // if (set.key==="2018_doldenset") console.log("MERGE",b,value);
     switch (b.type) {
       case 'skill':
       case 'attribute':
