34c453866ef935cc84fcf2227bac7673375021cf
Uwe Ohse <uwe@ohse.de>
2023-05-12 13:17:08 +0000

    add craft/bid links to quest display

--- quest.js
+++ quest.js
@@ -1,6 +1,26 @@
 // vim: tabstop=2 shiftwidth=2 expandtab
 //
 TWDS.quest = {}
+TWDS.quest.getMinimapLink = function (req) {
+  if (!req) return ''
+  const li = document.createElement('span')
+  li.className = 'TWDS_minimaplink'
+  if (TWDS.settings.quest_add_util_buttons) {
+    if (req.type === 'inventory_changed') {
+      let x = TWDS.itemAnyCraftButton(req.id)
+      if (x) { li.appendChild(x) }
+      x = TWDS.itemBidButton(req.id)
+      if (x) {
+        li.appendChild(x)
+      }
+    } else {
+      console.log('unhandled', req.type, req)
+    }
+  }
+  const old = Quest.prototype._TWDS_backup_getMinimapLink(req)
+  return li.outerHTML + old
+}
+
 TWDS.quest.render = function (requirement, clsFinish) {
   Quest.prototype._TWDS_backup_render.apply(this, arguments)
   const details = this.el
@@ -10,17 +30,17 @@ TWDS.quest.render = function (requirement, clsFinish) {
       for (let i = 0; i < this.requirements.length; i++) {
         const req = this.requirements[i]
         if (req.jsInfo && req.jsInfo.type === 'inventory_changed') {
-          const item = Bag.getItemByItemId(req.jsInfo.id)
-          if (item) {
+          const li = $('ul.requirement_container li:nth-child(' + (i + 1) + ')', details)
+          const trackerli = $('ul.requirement_container li:nth-child(' + (i + 1) + ')', tracker)
+          const bagitem = Bag.getItemByItemId(req.jsInfo.id)
+          if (bagitem) {
             if (TWDS.settings.quest_show_itemcount) {
-              const sp = $("<span class='TWDS_quest_itemcount'> [" + item.count + ']</span>')
-              const li = $('ul.requirement_container li:nth-child(' + (i + 1) + ')', details)
+              const sp = $("<span class='TWDS_quest_itemcount'> [" + bagitem.count + ']</span>')
               li.append(sp)
             }
             if (TWDS.settings.questtracker_show_itemcount) {
-              const sp = $("<span class='TWDS_questtracker_itemcount'> [" + item.count + ']</span>')
-              const li = $('ul.requirement_container li:nth-child(' + (i + 1) + ')', tracker)
-              li.append(sp)
+              const sp = $("<span class='TWDS_questtracker_itemcount'> [" + bagitem.count + ']</span>')
+              trackerli.append(sp)
             }
           }
         }
@@ -77,18 +97,23 @@ TWDS.registerSetting('bool', 'quest_cancel_question',
 TWDS.registerSetting('bool', 'quest_show_itemcount',
   TWDS._('QUESTS_SETTING_SHOW_ITEMCOUNT', 'Show the amount of items in your inventory in the quest window'),
   false, null, 'Quests', null, 2)
+TWDS.registerSetting('bool', 'quest_add_util_buttons',
+  TWDS._('QUESTS_SETTING_ADD_UTIL_BUTTONS', 'Add utility functions to the quest window'),
+  false, null, 'Quests', null, 3)
 TWDS.registerSetting('bool', 'questtracker_show_itemcount',
   TWDS._('QUESTS_SETTING_SHOW_ITEMCOUNT_TRACKER', 'Show the amount of items in your inventory in the quest tracker.'),
-  false, null, 'Quests', null, 3)
+  false, null, 'Quests', null, 4)
 TWDS.registerSetting('bool', 'questtracker_show_booklinks',
   TWDS._('QUESTS_SETTING_ADD_BOOK_LINK', 'Add quest book links to the quest tracker'),
-  true, null, 'Quests', null, 4)
+  true, null, 'Quests', null, 5)
 
 TWDS.registerStartFunc(function () {
   Quest.prototype._TWDS_backup_render = Quest.prototype.render
   Quest.prototype.render = TWDS.quest.render
   Quest.prototype._TWDS_backup_getQuestTrackerEl = Quest.prototype.getQuestTrackerEl
   Quest.prototype.getQuestTrackerEl = TWDS.quest.getQuestTrackerEl
+  Quest.prototype._TWDS_backup_getMinimapLink = Quest.prototype.getMinimapLink
+  Quest.prototype.getMinimapLink = TWDS.quest.getMinimapLink
   window.QuestWindow._TWDS_backup_cancelQuest = window.QuestWindow.cancelQuest
   window.QuestWindow.cancelQuest = TWDS.quest.cancelQuest
 })
