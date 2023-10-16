b820f75cc5418698b3376b6b556668bdd90ae0a3
Uwe Ohse <uwe@ohse.de>
2023-10-16 15:23:32 +0000

    market: make bulk mode help removable

--- market.js
+++ market.js
@@ -209,12 +209,15 @@ TWDS.marketwindow.enhanceit = function (thing) {
   }
 
   // bulk mode
-  const oo = document.querySelector('#mps_otheroffers')
-  if (oo) {
-    const p = oo.parentNode
+  const h4 = TWDS.q1('#market_createoffer_window .txcenter > span')
+  if (h4) {
+    let prev=h4.previousSibling
+    if (prev.nodeType===1 && prev.nodeName==="BR") {
+      prev.remove();
+    }
     TWDS.createEle({
       nodeName: 'div',
-      last: p,
+      beforebegin: h4,
       className: 'TWDS_market_bulkmode_container ' + (TWDS.marketwindow.bulkmodeactive ? 'TWDS_market_bulkmode_active' : ''),
       children: [
         {
@@ -234,12 +237,36 @@ TWDS.marketwindow.enhanceit = function (thing) {
         }, {
           nodeName: 'b',
           textContent: TWDS._('MARKETWINDOW_BULKMODE', 'bulk mode')
-        }, {
-          nodeName: 'div',
-          innerHTML: TWDS._('MARKETWINDOW_BULKMODE_HELP', 'Inventory and sale list will updated after a pause of some seconds.<br>This seriously reduces the number of streak of bad luck events.')
         }
       ]
     })
+  }
+  const oo = document.querySelector('#mps_otheroffers')
+  if (oo) {
+    const p = oo.parentNode
+    if (!TWDS.settings.market_bulkmode_help_read) {
+      TWDS.createEle({
+        nodeName: 'div',
+        className: 'TWDS_market_bulkmode_help',
+        last: p,
+        style: { border:"1px solid #000"},
+        children: [
+          {
+            nodeName: "div",
+            innerHTML: TWDS._('MARKETWINDOW_BULKMODE_HELP', 'bulk mode: inventory and sale list will updated after a pause of some seconds.<br>This seriously reduces the number of streak of bad luck events.')
+          },
+          {
+            nodeName: "button",
+            textContent: TWDS._("MARKETWINDOW_BULKMODE_READ","read and understand, now remove the help text"),
+            onclick: function() {
+              this.closest(".TWDS_market_bulkmode_help").remove();
+              TWDS.settings.market_bulkmode_help_read=true;
+              TWDS.saveSettings()
+            }
+          }
+        ]
+      });
+    }
   }
   // +1/2 feature
   const mmb = document.querySelector('#market_min_bid')
