19f2468c957c237af43d837ce984c026153d6d51
Uwe Ohse <uwe@ohse.de>
2023-11-07 21:38:03 +0000

    fix stupid bug

--- fbmisc.js
+++ fbmisc.js
@@ -1,42 +1,42 @@
 // vim: tabstop=2 shiftwidth=2 expandtab
-TWDS.fbmisc= {}
-TWDS.fbmisc.shownumberinterval=0
-TWDS.fbmisc.shownumber=function() {
-  Ajax.remoteCall("fort_overview", "", {}, function(data) {
-    console.log("ajax gave",data, data.js);
-    let mp=TWDS.q1("#ui_bottombar .multiplayer");
-    if (!mp) return;
-    console.log("mp found",mp);
+TWDS.fbmisc = {}
+TWDS.fbmisc.shownumberinterval = 0
+TWDS.fbmisc.shownumber = function () {
+  Ajax.remoteCall('fort_overview', '', {}, function (data) {
+    console.log('ajax gave', data, data.js)
+    const mp = TWDS.q1('#ui_bottombar .multiplayer')
+    if (!mp) return
+    console.log('mp found', mp)
 
-    let ele=TWDS.q1(".TWDS_fbcount",mp);
-    console.log("ele?",ele);
+    let ele = TWDS.q1('.TWDS_fbcount', mp)
+    console.log('ele?', ele)
 
-    let n=0
+    let n = 0
     if (data.js) {
-      n=data.js.length
+      n = data.js.length
     }
-    console.log("n?",n);
+    console.log('n?', n)
     if (ele && !n) {
-      ele.remove();
-      return;
+      ele.remove()
+      return
     }
     if (n && !ele) {
-      ele=TWDS.createEle({
-        nodeName:"span.TWDS_fbcount",
-        last:mp,
-      });
+      ele = TWDS.createEle({
+        nodeName: 'span.TWDS_fbcount',
+        last: mp
+      })
     }
-    ele.textContent=n
+    ele.textContent = n
   })
 }
-TWDS.fbmisc.shownumberstarter=function() {
+TWDS.fbmisc.shownumberstarter = function () {
   if (TWDS.shownumberinterval) {
-    window.clearInterval(TWDS.shownumberinterval);
-    TWDS.shownumberinterval=0
+    window.clearInterval(TWDS.shownumberinterval)
+    TWDS.shownumberinterval = 0
   }
-  if (TWDS.setting.fbmisc_fbcount) {
-    TWDS.fbmisc.shownumber();
-    TWDS.shownumberinterval=window.setInterval(TWDS.shownumberinterval, 5*60*1000);
+  if (TWDS.settings.fbmisc_fbcount) {
+    TWDS.fbmisc.shownumber()
+    TWDS.shownumberinterval = window.setInterval(TWDS.shownumberinterval, 5 * 60 * 1000)
   }
 }
 
