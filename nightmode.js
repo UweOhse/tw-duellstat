c21cb81a3d0dcbb3d48e5766427d12c8fee236d9
Uwe Ohse <uwe@ohse.de>
2024-01-18 15:09:33 +0000

    before/after->start/end, it's clearer

--- nightmode.js
+++ nightmode.js
@@ -29,21 +29,17 @@ TWDS.nightmode.setit = function (force) {
 
   const h = (new Date()).getHours()
   let doit = 0
-  if (TWDS.settings.nightmode_before !== -1) {
-    if (h < TWDS.settings.nightmode_before) {
-      doit = 1
-    }
+  const start = parseInt(TWDS.settings.nightmode_start)
+  const end = parseInt(TWDS.settings.nightmode_end)
+
+  if (h >= start && h < end) {
+    doit = 1 // australia case - 9-18
+  } else if (end < start) {
+    // europe case 21-06
+    if (h >= start) doit = 1 // subcase 21..23
+    else if (h < end) doit = 1 // subcase 00..06
   }
-  if (TWDS.settings.nightmode_after !== -1) {
-    if (h >= TWDS.settings.nightmode_after) {
-      doit = 1
-      if (TWDS.settings.nightmode_before !== -1) {
-        if (h >= TWDS.settings.nightmode_before) {
-          doit = 0
-        }
-      }
-    }
-  }
+
   if (force !== undefined) {
     console.log('forcing it from', doit, 'to', force)
     doit = force
@@ -62,19 +58,19 @@ TWDS.nightmode.timer = function () {
   TWDS.nightmode.timeout = setTimeout(TWDS.nightmode.timer, todo * 1000 + 1000)
 }
 TWDS.registerStartFunc(function () {
-  TWDS.registerSetting('int', 'nightmode_after',
-    TWDS._('NIGHTMODE_AFTER',
-      'Switch to the night mode from that hour on. -1: off'),
-    { default:-1, min:-1, max:23, },
+  TWDS.registerSetting('int', 'nightmode_start',
+    TWDS._('NIGHTMODE_START',
+      'Start the night mode at that hour. -1: off'),
+    { default: -1, min: -1, max: 23 },
     function () { TWDS.nightmode.timer() }, 'nightmode', null, 1)
-  TWDS.registerSetting('int', 'nightmode_before',
-    TWDS._('NIGHTMODE_BEFORE',
-      'Turn night mode off from that hour on. -1: off'),
-    { default:-1, min:-1, max:23},
+  TWDS.registerSetting('int', 'nightmode_end',
+    TWDS._('NIGHTMODE_END',
+      'End night mode off at that hour. -1: off'),
+    { default: -1, min: -1, max: 23 },
     function () { TWDS.nightmode.timer() }, 'nightmode', null, 2)
   TWDS.registerSetting('int', 'nightmode_brightness',
     TWDS._('NIGHTMODE_BRIGHTNESS',
-      'Set night mode brightness (in percent).'),
+      'Set night mode brightness (in percent). 75 might be a good start.'),
     { default: 100, min: 33, max: 133 }
     , function () { TWDS.nightmode.stylehandler() }, 'nightmode', null, 3)
   TWDS.nightmode.timer()
