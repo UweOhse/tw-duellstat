e087800dc01a21e7c3b3ee1032c84bbbe82ffff4
Uwe Ohse <uwe@ohse.de>
2024-09-23 12:27:26 +0000

    async recipe/crafting loader

--- crafting.js
+++ crafting.js
@@ -6,15 +6,13 @@ TWDS.crafting = {}
 TWDS.crafting.mycraftableitems = {}
 TWDS.crafting.mycraftresources = {}
 TWDS.crafting.myrecipes = {}
-TWDS.crafting.callback = null
+TWDS.crafting.ready = false
+TWDS.crafting.running = false
 
-TWDS.crafting.storemyrecipes = function (x) {
-  const cb = TWDS.crafting.callback
-  TWDS.crafting.callback = null
-  if (x.error) return
-  if (x.recipes_content) {
-    for (let i = 0; i < x.recipes_content.length; i++) {
-      const rid = x.recipes_content[i].item_id
+TWDS.crafting.storemyrecipes = function (json) {
+  if (json.recipes_content) {
+    for (let i = 0; i < json.recipes_content.length; i++) {
+      const rid = json.recipes_content[i].item_id
       const r = ItemManager.get(rid)
       if (r) {
         TWDS.crafting.mycraftableitems[r.craftitem] = true
@@ -23,24 +21,43 @@ TWDS.crafting.storemyrecipes = function (x) {
           TWDS.crafting.mycraftresources[ri] = true
         }
       }
-      TWDS.crafting.myrecipes[rid] = x.recipes_content[i].last_craft
+      TWDS.crafting.myrecipes[rid] = json.recipes_content[i].last_craft
     }
   }
-  if (cb) {
-    cb()
-  }
-}
-TWDS.crafting.start = function (cb) {
-  if (cb) { TWDS.crafting.callback = cb }
-  if (!ItemManager.isLoaded()) {
-    window.setTimeout(TWDS.crafting.start, 250)
-    return
-  }
-  Ajax.remoteCall('crafting', '', {}, TWDS.crafting.storemyrecipes)
 }
 
-TWDS.registerStartFunc(function () {
-  setTimeout(TWDS.crafting.start, 2500)
-})
+TWDS.crafting.asyncloader = function(resolve,reject) {
+  if (TWDS.crafting.ready) {
+    if (resolve) {
+      resolve("loaded in another thread")
+      return;
+    }
+    return Promise.resolve("using already loaded result");
+  }
+  if (TWDS.crafting.running) {
+    if (resolve) {
+      setTimeout(TWDS.crafting.asyncloader,250,resolve,reject);
+      return;
+    }
+    return new Promise(function(resolve,reject) {                                                                                      
+      setTimeout(TWDS.crafting.asyncloader,250,resolve,reject);
+    });
+  }
+  TWDS.crafting.running=1
+  return new Promise(function(resolve,reject) {                                                                                      
+    Ajax.remoteCall('crafting', '', {}, function(json) {
+      if (json.error) {
+        reject(new Error(json.error));
+      }
+      TWDS.crafting.storemyrecipes(json);
+      TWDS.crafting.ready=1
+      resolve("loaded");
+    });
+  });
+}
+TWDS.crafting.reset = function () {
+  TWDS.crafting.running=0
+  TWDS.crafting.ready=0
+}
 
 // vim: tabstop=2 shiftwidth=2 expandtab
