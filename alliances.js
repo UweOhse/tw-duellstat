b575082833813555371464063a5e91d69e5a2f64
Uwe Ohse <uwe@ohse.de>
2024-09-23 11:36:39 +0000

    do not load alliance data on startup anymore

--- alliances.js
+++ alliances.js
@@ -4,12 +4,15 @@ TWDS.alliances = {}
 TWDS.alliances.id2name = {}
 TWDS.alliances.townid2allianceid = {}
 TWDS.alliances.ready = false
+TWDS.alliances.running = false
 
-TWDS.alliances.getdata = function () {
+TWDS.alliances.getdataworker = function (resolve, reject) {
   Ajax.get('map', 'get_minimap', {}, function (json) {
-    // console.log("MM",json);
     if (json.error) {
-      return new UserMessage(json.msg).show()
+      reject(new Error(json.error))
+      console.log('TWDS.alliances: ran into', json.error, 'during get_minimap')
+      TWDS.alliances.running = false
+      return new UserMessage(json.error).show()
     }
     const todo = []
     for (const t of Object.values(json.towns)) {
@@ -27,15 +30,23 @@ TWDS.alliances.getdata = function () {
     }
     let run = 0
     const doone = function (i) {
-      if (i > todo.length) {
+      if (i >= todo.length) {
         TWDS.alliances.ready = true
+        TWDS.alliances.running = false
+        resolve('loaded')
         return
       }
       const id = todo[i]
       Ajax.remoteCallMode('alliance', 'get_data', {
         alliance_id: id
       }, function (r) {
-        if (r.error === false && r.data && r.data.allianceName) {
+        if (r.error) {
+          TWDS.alliances.running = false
+          console.log('TWDS.alliances: ran into', json.error, 'during alliances get_data')
+          reject(new Error(json.error))
+          return new UserMessage(json.error).show()
+        }
+        if (r.data && r.data.allianceName) {
           TWDS.alliances.id2name[id] = r.data.allianceName
         }
         let delay = 250
@@ -49,10 +60,27 @@ TWDS.alliances.getdata = function () {
     doone(0)
   })
 }
-TWDS.alliances.startfunc = function () {
-  TWDS.alliances.getdata()
+TWDS.alliances.getdata = function (resolve, reject) {
+  if (TWDS.alliances.ready) {
+    if (resolve) {
+      resolve('its ready')
+      return
+    }
+    return Promise.resolve('already loaded')
+  }
+  if (TWDS.alliances.running) {
+    if (!resolve) {
+      return new Promise(function (resolve, reject) {
+        setTimeout(TWDS.alliances.getdata, 250, resolve, reject)
+      })
+    }
+    setTimeout(TWDS.alliances.getdata, 250, resolve, reject)
+    return
+  }
+  TWDS.alliances.running = true
+  return new Promise(function (resolve, reject) {
+    TWDS.alliances.getdataworker(resolve, reject)
+  })
 }
-
-TWDS.registerStartFunc(TWDS.alliances.startfunc)
 
 // vim: tabstop=2 shiftwidth=2 expandtab
