6cc9b0d93bda87007f29b4abaaccc2775b307332
Uwe Ohse <uwe@ohse.de>
2024-09-27 07:20:02 +0000

    add hack to have access to GM.xmlHttpRequest in the game, for questexporter

--- prefix.js
+++ prefix.js
@@ -4,15 +4,22 @@
 // @name:en     Clothcache (ex-Duellstat)
 // @author      xyzabcd
 // @namespace   xyzabcd
-// @description create a duell statistik
+// @description Enhance the usability of the game
 // @include https://*.the-west.*/game.php*
 // @include https://*.tw.innogames.*/game.php*
+// @grant GM.xmlHttpRequest
+// @connect support.innogames.com
 // @downloadURL https://ohse.de/uwe/tw-duellstat/release/tw-duellstat.user.js
 
 // @version     @REPLACEMYVERSION@
-// @grant       none
 // ==/UserScript==
 
+try {
+	unsafeWindow._TWDS_GM_XHR=GM.xmlHttpRequest
+	console.log("unsafeWindow._TWDS_GM_XHR", unsafeWindow._TWDS_GM_XHR);
+} catch(e) {
+	console.error("could not export GM.xmlHttpRequest to unsafeWindow", e);
+}
 (function(fn) {
   var script = document.createElement('script');
   script.setAttribute('type', 'application/javascript');
