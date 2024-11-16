3751f12d49b870e60a7abdb0c35aeea97a432c0f
Uwe Ohse <uwe@ohse.de>
2024-11-16 11:50:02 +0000

    hue-rotate option of the thread icon, and only jump to the end of the last page (forum)

--- forum.js
+++ forum.js
@@ -9,6 +9,10 @@ TWDS.forum.onload = function (ev, data) {
   const overview = TWDS.q1('#thread_overview', body)
   if (!overview) {
     // we are in a thread
+    const blp = TWDS.q1('.fancytable.thread .button.lastPage', body)
+    if (!blp) { return }
+    if (blp.style.display !== 'none') { return }
+    // we are on the last page, jump to the end.
     const rows = TWDS.q('.fancytable.thread .trows .tbody .rows .row', body)
     if (rows.length) {
       setTimeout(function () {
@@ -30,36 +34,41 @@ TWDS.forum.onload = function (ev, data) {
 
   const rows = TWDS.q('.row', body)
   rows.forEach(function (row) {
-    const cell4 = TWDS.q1('.cell_4', row)
-    if (!cell4) return
+    if (TWDS.settings.forum_iconcolorrotate) {
+      const cell0a = TWDS.q1('.cell_0 a', row)
+      if (cell0a) {
+        cell0a.style.filter = 'hue-rotate(90deg)'
+      }
+    }
+    if (TWDS.settings.forum_lastpage) {
+      const cell4 = TWDS.q1('.cell_4', row)
+      if (!cell4) return
 
-    // var r = e(this).find(".cell_1 a").attr("onclick").match(/\d+/);
-    const a = TWDS.q1('.cell_1 a', row)
-    const thread = a.onclick.toString().match(/\d+/)
+      // var r = e(this).find(".cell_1 a").attr("onclick").match(/\d+/);
+      const a = TWDS.q1('.cell_1 a', row)
+      const thread = a.onclick.toString().match(/\d+/)
 
-    // e(this).find(".cell_3").append('<img src="' + TWDB.images.lastpost + '" class="twdb_lastpost" style="position:absolute;cursor:pointer;margin-left:3px;" onclick="Forum.openThread(' + r + ", " + n + ')"></img>')
-    const count = cell4.textContent
-    cell4.innerHTML = "<a class='TWDS_lastpost' onclick='Forum.openThread(" + thread + ",\"last\",\"#quickreply\")'>" + count + '</a>'
+      // e(this).find(".cell_3").append('<img src="' + TWDB.images.lastpost + '" class="twdb_lastpost" style="position:absolute;cursor:pointer;margin-left:3px;" onclick="Forum.openThread(' + r + ", " + n + ')"></img>')
+      const count = cell4.textContent
+      cell4.innerHTML = "<a class='TWDS_lastpost' onclick='Forum.openThread(" + thread + ",\"last\",\"#quickreply\")'>" + count + '</a>'
+    }
   })
 }
-TWDS.forum.init = function (active) {
-  window.ForumWindow.TWDS_backup_open = window.ForumWindow.TWDS_backup_open || window.ForumWindow.open
-  if (active) {
-    window.ForumWindow.open = function () {
-      const out = window.ForumWindow.TWDS_backup_open.apply(this, arguments)
-      // $("iframe[src='forum.php']").on("load", 0, i)
-      const iframe = TWDS.q1("iframe[src='forum.php']")
-      if (iframe) {
-        iframe.onload = TWDS.forum.onload
-      }
-      return out
-    }
-  } else {
-    window.ForumWindow.open = window.ForumWindow.TWDS_backup_open
+TWDS.forum.open = function () {
+  const out = window.ForumWindow.TWDS_backup_open.apply(this, arguments)
+  const iframe = TWDS.q1("iframe[src='forum.php']")
+  if (iframe) {
+    iframe.onload = TWDS.forum.onload
   }
+  return out
 }
 TWDS.registerStartFunc(function () {
   TWDS.registerSetting('bool', 'forum_lastpage',
     TWDS._('FORUM_SETTING_LASTPAGE', 'Add a link to the last page of a forum thread.'),
-    true, TWDS.forum.init)
+    true)
+  TWDS.registerSetting('bool', 'forum_iconcolorrotate',
+    TWDS._('FORUM_SETTING_ICONCOLORROTATE', 'Rotate the color of the town forum thread icon by 90 degrees.'),
+    false)
+  window.ForumWindow.TWDS_backup_open = window.ForumWindow.TWDS_backup_open || window.ForumWindow.open
+  window.ForumWindow.open = TWDS.forum.open
 })
